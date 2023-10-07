19534c7f9a72c6fd3920908f7094f0b7adbf4843
laizu akram <laizuakram98@gmail.com>
2023-10-07 19:06:45 +0600

    create professional skills bar

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+// type js function
 var typed = new Typed("#text",{
     strings: ["Frontend Developer", "YouTuber", "Web Developer"],
     typeSpeed:100,
@@ -8,8 +9,17 @@ var typed = new Typed("#text",{
 
 });
 
+
+// radial animate function
 let number = document.getElementById('number');
+let number2 = document.getElementById('number2');
+let number3 = document.getElementById('number3');
+let number4 = document.getElementById('number4');
+
 let counter = 0;
+let counter2 = 0;
+let counter3 = 0;
+let counter4 = 0;
 
 setInterval(() =>{
     if(counter == 65){
@@ -19,4 +29,37 @@ setInterval(() =>{
         number.innerHTML = counter + '%';
     }
     
-},30)
+},25)
+
+// problem solving
+setInterval(() =>{
+    if(counter2 == 87){
+        clearInterval();
+    }else{
+        counter2 += 1;
+        number2.innerHTML = counter2 + '%';
+    }
+    
+},23)
+
+//communication
+setInterval(() =>{
+    if(counter3 == 71){
+        clearInterval();
+    }else{
+        counter3 += 1;
+        number3.innerHTML = counter3 + '%';
+    }
+    
+},25)
+
+//creativity
+setInterval(() =>{
+    if(counter4 == 63){
+        clearInterval();
+    }else{
+        counter4 += 1;
+        number4.innerHTML = counter4 + '%';
+    }
+    
+},25)
